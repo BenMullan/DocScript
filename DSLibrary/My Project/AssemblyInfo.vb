f428e42ef03a623ce55d0aa872843ea67391a858
BenMullan <BenMullan@Outlook.De>
2023-06-17 11:13:25 +0100

    Added MultiThreading (`DS_InterpretInNewThread`) and lightweight Web Server (`System_HostWebServerForString`).

--- DSLibrary/My Project/AssemblyInfo.vb
+++ DSLibrary/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.2.5.1")> 
-<Assembly: AssemblyFileVersion("4.2.5.1")> 
+<Assembly: AssemblyVersion("4.4.2.1")> 
+<Assembly: AssemblyFileVersion("4.4.2.1")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-GB")> 
