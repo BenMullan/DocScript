33656a5dd77fe4ea16ab122b9422d283fea57009
BenMullan <BenMullan@Outlook.De>
2023-06-21 08:50:21 +0100

    Improved command-line XML Syntax Highlighting (`DSCLI`)

--- DSCommandLineInterpreter/My Project/AssemblyInfo.vb
+++ DSCommandLineInterpreter/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.4.1.1")> 
-<Assembly: AssemblyFileVersion("4.4.1.1")> 
+<Assembly: AssemblyVersion("4.6.2.1")> 
+<Assembly: AssemblyFileVersion("4.6.2.1")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-GB")> 
