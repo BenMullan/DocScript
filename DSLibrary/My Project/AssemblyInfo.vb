cd24dd0855eeca08a25bb3e5d3f7a6850dfd00d2
BenMullan <BenMullan@outlook.de>
2024-06-09 09:18:21 +0100

    2024 Clean-up

--- DSLibrary/My Project/AssemblyInfo.vb
+++ DSLibrary/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("5.6.0.0")> 
-<Assembly: AssemblyFileVersion("5.6.0.0")> 
+<Assembly: AssemblyVersion("5.7.0.0")> 
+<Assembly: AssemblyFileVersion("5.7.0.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-GB")> 
